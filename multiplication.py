5efccacfc28ca9a9f012050ec1ccc38a0aebcf3d
disha <garaddisha@gmail.com>
2025-01-15 22:16:51 +0530

    multiplication table

---
+++ multiplication.py
@@ -0,0 +1,7 @@
+# num = int(input("display multiplication table of:"))
+# for i in range(1,11):
+#     print(f"{num} x {i} = {num*i}")
+
+mul = 4
+for i in range(1,11):
+    print(f"{mul} x {i} = {mul*i}")
